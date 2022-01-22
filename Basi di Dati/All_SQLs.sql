59ffefe5c11d740820f2c06cb67127042fc05d0c
Oleksandr Sosovskyy <97637922+Asashoryu@users.noreply.github.com>
2022-01-22 18:37:15 +0100

    Aggiunto primo trigger

--- Basi di Dati/All_SQLs.sql
+++ Basi di Dati/All_SQLs.sql
@@ -1,8 +1,8 @@
 --Al fine di evitare la distruzione e poi la ricreazione del database, si suggerisce di usare la seguente sequenza di comandi (togliendo i commenti)
---DROP SCHEMA public CASCADE;
---CREATE SCHEMA public;
---GRANT ALL ON SCHEMA public TO postgres;
---GRANT ALL ON SCHEMA public TO public;
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
 
 --Crea la tabella Rubrica
 CREATE TABLE Rubrica(
@@ -21,7 +21,8 @@ Rubrica_FK VARCHAR(30) NOT NULL
 
 ALTER TABLE Gruppo
  ADD CONSTRAINT gruppo_pk PRIMARY KEY(Gruppo_ID),
- ADD CONSTRAINT gruppo_rubrica_fk FOREIGN KEY(Rubrica_FK) REFERENCES Rubrica(Utente_ID);
+ ADD CONSTRAINT gruppo_rubrica_fk FOREIGN KEY(Rubrica_FK) REFERENCES Rubrica(Utente_ID)
+ ON DELETE CASCADE;
  
 --Crea la tabella Contatto
 CREATE TABLE Contatto(
@@ -35,34 +36,40 @@ Rubrica_FK VARCHAR(20) NOT NULL
 
 ALTER TABLE Contatto
  ADD CONSTRAINT contatto_pk PRIMARY KEY(Contatto_ID),
- ADD CONSTRAINT contatto_rubica_fk FOREIGN KEY(Rubrica_FK) REFERENCES Rubrica(Utente_ID);
+ ADD CONSTRAINT contatto_rubica_fk FOREIGN KEY(Rubrica_FK) REFERENCES Rubrica(Utente_ID)
+ ON DELETE CASCADE;
 
 --Crea la tabella Account
 CREATE TABLE Account(
 Account_ID SERIAL ,
 Fornitore VARCHAR(20) NOT NULL,
- --L'indirizzo email deve essere strutturato in quest'ordine: testo,"@",testo,".",testo utilizzando un CHECK per verificare tale condizione
-IndirizzoEmail VARCHAR(256) NOT NULL UNIQUE CHECK ( IndirizzoEmail LIKE '_%@_%.__%'),
+
+IndirizzoEmail VARCHAR(256) NOT NULL UNIQUE,
 FraseStato VARCHAR(256),
 Nickname VARCHAR(30)
 );
 
 ALTER TABLE Account
- ADD CONSTRAINT account_pk PRIMARY KEY(Account_ID);
+ ADD CONSTRAINT account_pk PRIMARY KEY(Account_ID),
+ --L'indirizzo email deve essere strutturato in quest'ordine: testo,"@",testo,".",testo utilizzando un CHECK per verificare tale condizione
+ ADD CONSTRAINT correct_email_formatting CHECK ( IndirizzoEmail LIKE '_%@_%.__%');
 
 --Crea la tabella Email
 CREATE TABLE Email(
 Email_ID SERIAL,
- --L'indirizzo email deve essere strutturato in quest'ordine: testo,"@",testo,".",testo utilizzando un CHECK per verificare tale condizione
-IndirizzoEmail VARCHAR(256) NOT NULL UNIQUE CHECK (IndirizzoEmail LIKE '_%@_%.__%'),
+IndirizzoEmail VARCHAR(256) NOT NULL UNIQUE,
 Descrizione VARCHAR(30),
-Contatto_ID SERIAL
+Contatto_FK SERIAL
 );
 
 ALTER TABLE Email
  ADD CONSTRAINT email_pk PRIMARY KEY(Email_ID),
- ADD CONSTRAINT email_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID);
-
+ ADD CONSTRAINT email_contatto_fk FOREIGN KEY(Contatto_FK) REFERENCES Contatto(Contatto_ID)
+ ON DELETE CASCADE,
+ --L'indirizzo email deve essere strutturato in quest'ordine: testo,"@",testo,".",testo utilizzando un CHECK per verificare tale condizione
+ ADD CONSTRAINT correct_email_formatting CHECK (IndirizzoEmail LIKE '_%@_%.__%');
+ ADD CONSTRAINT not_redundant_email UNIQUE (Contatto_FK,indirizzoEmail)
+ 
 --Crea la tabella Indirizzo
 CREATE TABLE Indirizzo(
 Indirizzo_ID SERIAL ,
@@ -71,69 +78,78 @@ Città VARCHAR(30),
 Nazione VARCHAR(30) NOT NULL,
 CAP INTEGER NOT NULL,
 Descrizione VARCHAR(20) NOT NULL,
-Contatto_ID SERIAL
+Contatto_FK SERIAL
 );
 
 ALTER TABLE Indirizzo
  ADD CONSTRAINT indirizzo_pk PRIMARY KEY(Indirizzo_ID),
- ADD CONSTRAINT indirizzo_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID);
+ ADD CONSTRAINT indirizzo_contatto_fk FOREIGN KEY(Contatto_FK) REFERENCES Contatto(Contatto_ID)
+ ON DELETE CASCADE;
 
 --Crea la tabella Telefono
 CREATE TABLE Telefono (
 Telefono_ID SERIAL,
 Numero INTEGER NOT NULL,
 Descrizione VARCHAR(20),
-Contatto_ID SERIAL
+Contatto_FK SERIAL
 );
 
 ALTER TABLE Telefono
  ADD CONSTRAINT telefono_pk PRIMARY KEY(Telefono_ID),
- ADD CONSTRAINT telefono_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID);
+ ADD CONSTRAINT telefono_contatto_fk FOREIGN KEY(Contatto_FK) REFERENCES Contatto(Contatto_ID)
+ ON DELETE CASCADE,
+ ADD CONSTRAINT not_redundant_telephone_number UNIQUE(Contatto_FK,Numero);
 
 --Crea la tabella Associa 
 CREATE TABLE Associa (
-Contatto_ID SERIAL,
-Account_ID SERIAL
+Contatto_FK SERIAL,
+Account_FK SERIAL
 );
 
 ALTER TABLE Associa
- ADD CONSTRAINT associa_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
- ADD CONSTRAINT associa_account_fk FOREIGN KEY(Account_ID) REFERENCES Account(Account_ID);
-
---Crea la tabella RecapitoEmail
-CREATE TABLE RecapitoEmail(
-Contatto_ID SERIAL,
-Email_ID SERIAL
-);
-
-ALTER TABLE RecapitoEmail
- ADD CONSTRAINT recapitoemail_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
- ADD CONSTRAINT recapitoemail_email_fk FOREIGN KEY(Email_ID) REFERENCES Email(Email_ID);
-
---Crea la tabella RecapitoIndirizzo
-CREATE TABLE RecapitoIndirizzo(
-Contatto_ID SERIAL,
-Indirizzo_ID SERIAL
-);
-
-ALTER TABLE RecapitoIndirizzo
- ADD CONSTRAINT recapitoindirizzo_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
- ADD CONSTRAINT recapitoindirizzo_indirizzo_fk FOREIGN KEY(Indirizzo_ID) REFERENCES Indirizzo(Indirizzo_ID);
-
---Crea la tabella RecapitoTelefono
-CREATE TABLE RecapitoTelefono(
-Contatto_ID SERIAL,
-Telefono_ID SERIAL
-);
-ALTER TABLE RecapitoTelefono
- ADD CONSTRAINT recapitotelefono_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
- ADD CONSTRAINT recapitotelefono_telefono_fk FOREIGN KEY(Telefono_ID) REFERENCES Telefono(Telefono_ID);
+ ADD CONSTRAINT associa_contatto_fk FOREIGN KEY(Contatto_FK) REFERENCES Contatto(Contatto_ID)
+ ON DELETE CASCADE,
+ ADD CONSTRAINT associa_account_fk FOREIGN KEY(Account_FK) REFERENCES Account(Account_ID)
+ ON DELETE CASCADE;
 
 --Crea la tabella Composizione
 CREATE TABLE Composizione(
-Contatto_ID SERIAL,
-Gruppo_ID SERIAL
+Contatto_FK SERIAL,
+Gruppo_FK SERIAL
 );
 ALTER TABLE Composizione
- ADD CONSTRAINT composizione_contatto_fk FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
- ADD CONSTRAINT composizione_gruppo_fk FOREIGN KEY(Gruppo_ID) REFERENCES Gruppo(Gruppo_ID);
+ ADD CONSTRAINT composizione_contatto_fk FOREIGN KEY(Contatto_FK) REFERENCES Contatto(Contatto_ID)
+ ON DELETE CASCADE,
+ ADD CONSTRAINT composizione_gruppo_fk FOREIGN KEY(Gruppo_FK) REFERENCES Gruppo(Gruppo_ID)
+ ON DELETE CASCADE;
+
+
+--Definizione dei trigger per implementare i vincoli
+--I membri di un gruppo devono appartenere alla stessa rubrica
+CREATE OR REPLACE FUNCTION group_coherency_membership_f()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+	AS $$
+	DECLARE
+		r_contatto Rubrica.Utente_ID%TYPE;
+		r_gruppo Rubrica.Utente_ID%TYPE;
+	BEGIN
+		SELECT Rubrica_FK INTO r_contatto
+		FROM Contatto
+		WHERE Contatto_ID=NEW.Contatto_FK;
+		
+		SELECT Rubrica_FK INTO r_gruppo
+		FROM Gruppo
+		WHERE Gruppo_ID=NEW.Gruppo_FK;
+		
+		IF (r_contatto<>r_gruppo) THEN
+			RAISE NOTICE 'Il contatto di ID % non appartiene alla stessa rubrica del gruppo di ID %, pertanto l''inserimento è annullato ', NEW.Contatto_FK, NEW.Gruppo_FK;
+			RETURN OLD;
+		ELSE
+			RETURN NEW;
+		END IF;
+	END; $$;
+CREATE OR REPLACE TRIGGER group_coherency_membership
+	BEFORE INSERT ON Composizione
+	FOR EACH ROW
+	EXECUTE PROCEDURE group_coherency_membership_f();
