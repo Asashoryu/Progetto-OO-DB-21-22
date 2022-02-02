47bb8438bf0f93bb0cd2e8fb2f764490c5fc6748
Asashoryu <97637922+Asashoryu@users.noreply.github.com>
2022-02-02 23:13:28 +0100

    Inizio Object Orientation

---
+++ Object Orientation/src/gui/Home.java
@@ -0,0 +1,57 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+
+public class Home {
+
+	private JFrame frame;
+	private JTextField variabileTextField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home window = new Home();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Home() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		variabileTextField = new JTextField();
+		variabileTextField.setText("Inizio Rubrica");
+		panel.add(variabileTextField);
+		variabileTextField.setColumns(10);
+	}
+
+}
